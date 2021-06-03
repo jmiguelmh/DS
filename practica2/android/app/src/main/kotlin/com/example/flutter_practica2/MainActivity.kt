594a91c917fe6c9b7ac962aa9390938ee2400e92
Jose Miguel Marquez Herreros <jmiguelmh3000@gmail.com>
2021-06-03 23:07:58 +0200

    Borrado de directorios

--- practica2/android/app/src/main/kotlin/com/example/flutter_practica2/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.flutter_practica2
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- practica2/lib/Producto.dart
+++
@@ -1,9 +0,0 @@
-class Producto {
-  String nombre;
-  int cantidad;
-
-  Producto(String n, int c) {
-    this.nombre = n;
-    this.cantidad = c;
-  }
-}
